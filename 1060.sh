c583d5ac35ea125547f187e652f9a9a70f9f2461
HC <hcho0050@student.monash.edu>
2021-05-06 21:55:14 +1000

    testing

--- 1060.sh
+++ 1060.sh
@@ -3,4 +3,4 @@ git pull
 git add .
 read -p "commit message:" msg
 git commit -m "$msg"
-git push https://skjhfds:sZfjVYp978FR5pS@github.com
+git push https://skjhfds:sZfjVYp978FR5pS@github.com/ENG1060_Labs.git
