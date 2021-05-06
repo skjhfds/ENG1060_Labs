f3a56ba3ed102bf6a62deac7ea50157e3bee724e
HC <hcho0050@student.monash.edu>
2021-05-06 21:52:15 +1000

    test

--- 1060.sh
+++ 1060.sh
@@ -3,6 +3,4 @@ git pull
 git add .
 read -p "commit message:" msg
 git commit -m "$msg"
-git push
-Username for 'https://github.com': skjhfds
-Password for 'https://skjhfds@github.com': sZfjVYp978FR5pS
+git push https://skjhfds:sZfjVYp978FR5pS@github.com
