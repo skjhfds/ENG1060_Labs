6ca2990190aefb8434d75cd47ab754388b3086f9
yjuigf <hannah.en.tay@gmail.com>
2021-05-19 23:31:14 +1000

    <3

--- Assignment/Q2b.m
+++ Assignment/Q2b.m
@@ -37,4 +37,5 @@ time=@(f_rate) total_time(f_rate)-240;
 fprintf('The ideal conditions are: a feed rate of %.2f shovels/min, digging at\nevery location.\n\n',root2b);
 
 %% (iii) Determine final gold haul
-fprintf('Given that we exhaust the gold content at every location of this site,\nthe final gold haul is %.2fg.\n\n',sum(gw.*spd));
+recovered_gold2=sluice_recov(root2b)/100*6*sum(G2.*available_shov);
+fprintf('Given that we exhaust the gold content at every location of this site,\nthe final gold haul is %.2fg.\n\n',recovered_gold2);
