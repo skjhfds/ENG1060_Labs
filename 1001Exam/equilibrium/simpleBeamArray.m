605cf868b36d90a369f8674ff02153dc13c1bb03
Harry, CHOI <hcho0050@student.monash.edu>
2021-05-31 14:42:44 +1000

    did the diagram

--- 1001Exam/equilibrium/simpleBeamArray.m
+++ 1001Exam/equilibrium/simpleBeamArray.m
@@ -1,13 +1,72 @@
 function [lr,rr] = simpleBeamArray(L,mx)
-%function [lr,rr] = simpleBeamArray(L,mx)
+% function [lr,rr] = simpleBeamArray(L,mx)
 %
-%takes the length of a simply supported beam,
-%array of masses on a beam and array of each mass' distance
-%from the left support
+% Takes the length of a simply supported beam,
+% matrix of loads and distances from the left support on a beam
+% to calculate the reactions at both ends.
 %
+% Input arguments
+% -----------------
+%   L  length of beam
+%   mx matrix with column 1 loads, column 2 distance from left support
+% Output arguments
+% -----------------
+%  lr left support reaction
+%  rr right support reaction
+%
+% ------------x3-->
+% -------x2-->
+% --x1-->m    m    m
+% ====================
+% A                  O
+% <---------L-------->
+%
+
+
+% ---------------120m-->
+% --------40m-->
+% --3m-->3kN    10kN    5kN    
+% ===========================
+mx = sortrows(mx, 2);
+% Generate Diagram
+a = size(mx);
+d = [""];
+fprintf("Diagram:\n")
+for i = 1:a(1)
+    row = "    --";
+    if i > 1
+        for j = 1:strlength(d(1))-length(num2str(mx(i,2)))-10
+            row = row + "-";
+        end
+    end
+    row = row + mx(i,2) + "m-->";
+    if i ~= 1
+        row = row + "\n";
+    end
+    d(i) = row;
+    d(1) = d(1) + mx(i,1) + "kN    ";
+end
+fprintf(strjoin(fliplr(d),"")+"\n    ")
+for i = 1:strlength(d(1))-2
+    fprintf("=")
+end
+fprintf("\n    A")
+for i = 1:strlength(d(1))-4
+    fprintf(" ")
+end
+fprintf("O\n    <")
+iMax = strlength(d(1))-4-length(num2str(L));
+for i = 1:iMax
+    if i == round(iMax/2)
+        fprintf(L+"m")
+        continue
+    end
+    fprintf("-")
+end
+fprintf(">\n")
+
 
 %taking moments at LR
 rr = sum(mx(1).*mx(2))/L;
 lr = sum(mx(1))-rr;
 
-fprintf(
--- 1001Exam/main.m
+++ 1001Exam/main.m
@@ -1,7 +1,4 @@
 clc; clear; close all
 addpath(genpath('../1001Exam')); % load all folders
-a = [];
-for l = [0,2,4,5,6,8,10]
-    a(length(a)+1)=simpleBeam(10,80/100,l);
-end
-a
+A = [30,144420;3+5+4/2,2224;5,77773];
+simpleBeamArray(3+5+4,A);
