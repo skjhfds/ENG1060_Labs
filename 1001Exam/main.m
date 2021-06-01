6dd853aee185988ba1b74f42674ff58dcc814c29
Harry, CHOI <hcho0050@student.monash.edu>
2021-06-02 04:43:39 +1000

    213

--- 1001Exam/main.m
+++ 1001Exam/main.m
@@ -1,3 +1,11 @@
 clc; clear; close all
 addpath(genpath('../1001Exam')); % load all folders
-resultantBeamArray([triangleLoad(4,2);trapLoad(6,2,10,4);30,4+6+2],[],[20])
+
+fx=-10-20*cos(30*pi/180)
+fy = -20*sin(30*pi/180)
+sumZ = 10*2-15-20*sin(30*pi/180)*3+20*cos(30*pi/180)
+
+sumZ/fy
+
+hypot(fx,fy)
+atan(fy/fx)*180/pi
