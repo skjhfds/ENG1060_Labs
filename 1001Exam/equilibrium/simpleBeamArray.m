76e9b290520b87a9110d70dd9a10c6fa2757c4e3
Harry, CHOI <hcho0050@student.monash.edu>
2021-06-01 15:58:45 +1000

    upgraqded beamarray

--- 1001Exam/equilibrium/simpleBeamArray.m
+++ 1001Exam/equilibrium/simpleBeamArray.m
@@ -1,5 +1,5 @@
-function [lr,rr] = simpleBeamArray(L,mx)
-% function [lr,rr] = simpleBeamArray(L,mx)
+function [lr,rr] = simpleBeamArray(L,mx,my,mz)
+% function [lr,rr] = simpleBeamArray(L,mx,my,mz)
 %
 % Takes the length of a simply supported beam,
 % matrix of loads and distances from the left support on a beam
@@ -8,42 +8,72 @@ function [lr,rr] = simpleBeamArray(L,mx)
 % Input arguments
 % -----------------
 %   L  length of beam
-%   mx matrix with column 1 loads, column 2 distance from left support
+%   mx horizontal forces (column 1) and distances (column 2)
+%   my vertical forces (column 1) and distances (column 2)
+%   mz moments occuring along beam
 % Output arguments
 % -----------------
-%  lr left support reaction
+%  lr left support reactions
 %  rr right support reaction
 %
-fprintf('\n       simpleBeamArray()\n===============================\n')
-mx = sortrows(mx, 2);
+fprintf('\n       beamArray()\n===============================\n')
+if ~exist("my","var")
+    my = [];
+end
+if ~exist("mz","var")
+    mz = [];
+end
 % Generate Diagram
 a = size(mx);
-d = [""];
-fprintf("Diagram:\n")
+d0 = [""]; d1 = [""];
+fprintf("Diagram:\n      ")
+% vertical load
 for i = 1:a(1)
-    row = "    --";
-    if i > 1
-        for j = 1:strlength(d(1))-length(num2str(mx(i,2)))-10
-            row = row + "-";
-        end
-    end
-    row = row + mx(i,2) + "m-->";
-    if i ~= 1
-        row = row + "\n";
-    end
-    d(i) = row;
-    d(1) = d(1) + mx(i,1) + "kN    ";
+    d1(i) = "V " + mx(i)+"kN ";
 end
-fprintf(strjoin(fliplr(d),"")+"\n    ")
-for i = 1:strlength(d(1))-2
+% vertical distance
+for i = 1:a(1)
+    d0(i) = "| " + mx(i,2)+"m";
+end
+
+% horizontal load
+a = size(my);
+aa = length(d1);
+for i = 1:a(1)
+    d1(i+aa) = "--> " + my(i)+"kN ";
+end
+% horizontal distance
+for i = 1:a(1)
+    d0(i+aa) = "    " + my(i,2)+"m";
+end
+
+% match distances, d0 and d1
+for i = 1:length(d0)
+    while strlength(d0(i)) > strlength(d1(i))
+        d1(i) = d1(i) + " ";
+    end
+    while strlength(d0(i)) < strlength(d1(i))
+        d0(i) = d0(i) + " ";
+    end
+end
+
+d = strjoin(d1);
+% moments
+a = size(mz);
+for i = 1:a(1)
+    d = d + " Q " + mz(i)+"kN ";
+end
+fprintf(strjoin(d0) + "\n      ")
+fprintf(d+"\n    ")
+for i = 1:strlength(d)+2
     fprintf("=")
 end
 fprintf("\n    A")
-for i = 1:strlength(d(1))-4
+for i = 1:strlength(d)
     fprintf(" ")
 end
 fprintf("O\n    <")
-iMax = strlength(d(1))-4-length(num2str(L));
+iMax = strlength(d(1))-length(num2str(L));
 for i = 1:iMax
     if i == round(iMax/2)
         fprintf(L+"m")
@@ -54,19 +84,28 @@ end
 fprintf(">\n\n")
 
 %taking moments at LR
-rr = sum(mx(:,1).*mx(:,2))/L;
-lr = sum(mx(:,1))-rr;
+rr = (sum(mx(:,1).*mx(:,2)) + sum(my(:,1).*my(:,2)) + sum(mz))/L;
+lry = sum(mx(:,1))-rr;
+lrx = -sum(my(:,1));
+lr = [lrx, lry];
 
 %printing equations
 a=size(mx); 
 mzStr="    sum(mz) = ";
-
 for i=1:a(1)
     mzStr = mzStr + sprintf("-%g*%g ",mx(i,1),mx(i,2));
+end
+a=size(my);
+for i=1:a(1)
+    mzStr = mzStr + sprintf("-%g*%g ",my(i,1),my(i,2));
+end
+a=size(mz);
+for i=1:a(1)
+    mzStr = mzStr + sprintf("-%g ",mz(i));
 end
 
 mzStr=mzStr+sprintf("+ %g RR = 0",L);
 fprintf('Equations\nTaking moments from left end,\n')
 fprintf(mzStr)
-fprintf('\n         RR = %g\n   sum(F_y) = LR - %g + %g = 0\n         LR = %g',rr,sum(mx(:,1)),rr,lr)
-fprintf('\n\n Left Reaction: %gkN\nRight Reaction: %gkN\n',lr,rr)
+fprintf('\n         RR = %g\n   sum(F_y) = LRy - %g + %g = 0\n        LRy = %g\n   sum(F_x) = LRx + %g = 0\n        LRx = %g',rr,sum(mx(:,1)),rr,lry,sum(my(:,1)), lrx)
+fprintf('\n\n Left Reaction: %gkN (horizontal)\n                %gkN (vertical)\nRight Reaction: %gkN\n',lr,rr)
